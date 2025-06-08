38b378683c30087ab14fd7a1dd4277b3d1e98624
Vicho222 <vicemartinez@alumnos.uai.cl>
2025-06-08 15:38:13 -0400

    Comienzo ProcessorTask.hs

--- ProcessorTask.hs
+++ ProcessorTask.hs
@@ -1,1 +1,19 @@
+module ProcessorTask where
 
+import Types (Node(..), creaNodo)
+import Heuristics (manhattan, euclidean, chebyshev)
+import Debug.Trace (trace)
+
+-- Esta es la función que realiza el algoritmo A*
+-- Espera de parámetro la matriz, el punto de entrada start, el punto de salida end y la heurística
+-- Devuelve la lista de nodos que forman el camino
+astar :: [[Int]] -> (Int, Int) -> (Int, Int)-> (Node -> Node -> Float) -> [Node]
+astar matriz start end  heuristica =
+    let nodoInicio = creaNodo start (head (head matriz))
+        nodoMeta  = creaNodo end (matriz !! fst end !! snd end)
+        listaAbierta = [nodoInicio]
+        listaCerrada = []
+        currentNode = nodoInicio
+    in
+        -- trace ("Iniciando: " ++ show nodoMeta ++ "\n") $
+        cicloDelAlgoritmo matriz listaAbierta listaCerrada nodoMeta  heuristica
